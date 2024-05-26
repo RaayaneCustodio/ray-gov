7ad7a338df76e69dce10ce9150d10fccbd0d5ec6
Raayane Custodio <raayanecustodio@gmail.com>
2024-05-26 19:10:01 -0300

    comentando pagina para teste

--- app/dashboard/create-link/page.tsx
+++ app/dashboard/create-link/page.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+/*import { useState } from 'react';
 
 export default async function CreateLinkPage() {
-    const accessToken = ""
-    const userId = ""
-    const apiUrl = ``;
+    const accessToken = "EAAQeLBQkr3UBO0xyVB0yK4y7D5gqgAURz5lJbrY56hUuovyvXzZA7L0El6pZBlRysGORBM0adiPlH9XxZCcu0mPjBSiAJHCNcVY1HTcpZAY2CiewoLVCWARpVvuJZCVE3S3TAgBXUKOuA3LwEPloZA34WPMXwrdzk0rFeZBXRUodfjMHmuUeZABIfKP3Ll9E2Xlo9BMGuxZCJEkUduGOlpN03T2ufwiNZAWEle4QdByS06pJM4kZBSnQWhKZB3WW33ZBN4ZCoZD"
+    const userId = "3144917115638952"
+    const apiUrl = `https://graph.facebook.com/${userId}?fields=id,name,email,birthday&access_token=${accessToken}`;
 
    
     const fetchPost =  await fetch(apiUrl)
@@ -22,3 +22,4 @@ export default async function CreateLinkPage() {
         </div>
     )
 } 
+*/
