cee64de8d13fb8bf5990696c8ee401e66f1ac31f
Raayane Custodio <raayanecustodio@gmail.com>
2024-05-26 18:50:45 -0300

    retirando api do facebook

--- app/dashboard/create-link/page.tsx
+++ app/dashboard/create-link/page.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 
 export default async function CreateLinkPage() {
-    const accessToken = "EAAQeLBQkr3UBO0xyVB0yK4y7D5gqgAURz5lJbrY56hUuovyvXzZA7L0El6pZBlRysGORBM0adiPlH9XxZCcu0mPjBSiAJHCNcVY1HTcpZAY2CiewoLVCWARpVvuJZCVE3S3TAgBXUKOuA3LwEPloZA34WPMXwrdzk0rFeZBXRUodfjMHmuUeZABIfKP3Ll9E2Xlo9BMGuxZCJEkUduGOlpN03T2ufwiNZAWEle4QdByS06pJM4kZBSnQWhKZB3WW33ZBN4ZCoZD"
-    const userId = "3144917115638952"
-    const apiUrl = `https://graph.facebook.com/${userId}?fields=id,name,email,birthday&access_token=${accessToken}`;
-    // const apiUrl = `https://graph.facebook.com/v19.0/${postId}?fields=likes.summary(true),comments.summary(true),shares&access_token=${accessToken}`;
+    const accessToken = ""
+    const userId = ""
+    const apiUrl = ``;
+
    
     const fetchPost =  await fetch(apiUrl)
     const data = await fetchPost.json()
