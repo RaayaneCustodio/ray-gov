c08c52cd3f4ab813b736b13dd08e69bcae680ad7
Raayane Custodio <raayanecustodio@gmail.com>
2024-05-26 19:18:11 -0300

    editando

--- app/dashboard/create-link/page.tsx
+++ app/dashboard/create-link/page.tsx
@@ -1,25 +1,15 @@
-/*import { useState } from 'react';
+import React from 'react';
 
-export default async function CreateLinkPage() {
-    const accessToken = "EAAQeLBQkr3UBO0xyVB0yK4y7D5gqgAURz5lJbrY56hUuovyvXzZA7L0El6pZBlRysGORBM0adiPlH9XxZCcu0mPjBSiAJHCNcVY1HTcpZAY2CiewoLVCWARpVvuJZCVE3S3TAgBXUKOuA3LwEPloZA34WPMXwrdzk0rFeZBXRUodfjMHmuUeZABIfKP3Ll9E2Xlo9BMGuxZCJEkUduGOlpN03T2ufwiNZAWEle4QdByS06pJM4kZBSnQWhKZB3WW33ZBN4ZCoZD"
-    const userId = "3144917115638952"
-    const apiUrl = `https://graph.facebook.com/${userId}?fields=id,name,email,birthday&access_token=${accessToken}`;
-
-   
-    const fetchPost =  await fetch(apiUrl)
-    const data = await fetchPost.json()
-    console.log(data)
-
+export default function CreateLinkPage() {
     return (
         <div>
             <form action="">
                 <div className="grid gap-1">
                     <label htmlFor="link">Insira o link do post</label>
-                    <input type="url"  className="h-full border p-3 rounded"/>
+                    <input type="url" className="h-full border p-3 rounded" />
                 </div>
                 <button type="submit" className="mt-6 bg-slate-800 text-slate-100 px-5 py-3 rounded">Cadastrar link</button>
             </form>
         </div>
     )
 } 
-*/
