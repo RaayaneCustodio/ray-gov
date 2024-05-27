7e26b2847ae03acb9dbcce591c5d6920ec5438e8
Raayane Custodio <raayanecustodio@gmail.com>
2024-05-26 23:34:59 -0300

    teste nas imagens

--- app/(www)/layout.tsx
+++ app/(www)/layout.tsx
@@ -5,17 +5,17 @@ import { Header } from "../components/header"
 import { Footer } from "../components/footer"
 
 interface DashboardLayoutProps {
-	children: React.ReactNode;
-  }
-  
-  const DashboardLayout = ({ children }: DashboardLayoutProps) => {
-	return (
-	  <div className="min-h-screen flex flex-col bg-[#1a202c] text-white">
-		<Header />
-		<main className="flex-1 p-6">{children}</main>
-		<Footer />
-	  </div>
-	);
-  };
-  
-  export default DashboardLayout;
+  children: React.ReactNode;
+}
+
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#1a202c] text-white">
+      <Header />
+      <main className="flex-1 p-6">{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default DashboardLayout;
