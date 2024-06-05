416a3aa62fc07df3f607d06f1d41fa353467199e
Raayane Custodio <raayanecustodio@gmail.com>
2024-06-05 00:33:05 -0300

    Alteração nos usuarios logados

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -28,31 +28,57 @@ export default async function DashboardPage() {
             <div className="grid grid-cols-12 gap-4">
                 <div className="col-span-10 p-4">
                     <h2 className="text-2xl font-semibold mb-4">Bem-vindo, {user?.firstName}</h2>
-                    <div className="bg-white rounded shadow p-4">
-                        <h3 className="text-lg font-semibold mb-2">Usuários Registrados</h3>
-                        <ul>
-                            {users && users.length > 0 && users.map((user: any) => {
-                                const timestamp = new Date(user.email_addresses[0].created_at)
-                                const day = timestamp.getUTCDate();
-                                const month = timestamp.getUTCMonth() + 1; // Os meses em JavaScript são baseados em zero
-                                const year = timestamp.getUTCFullYear();
-                                const hours = timestamp.getUTCHours();
-                                const minutes = timestamp.getUTCMinutes();
-                                const formattedDate = `${day}-${month}-${year} ${hours}:${minutes}`;
-                                return (
-                                    // <pre>{JSON.stringify(users,null,2)}</pre>
-
-                                    <li key={user.id} className="flex gap-2 border-b py-2">
-                                        <img src={user.profile_image_url} alt="Foto de perfil" className="w-10 h-10 rounded-full mr-2" />
-                                        <div>
-                                            <span className="block text-gray-800">{user.first_name} {user.last_name}</span>
-                                            <span className="block text-gray-600">{user.email_addresses[0].email_address}</span>
-                                            <span className="block text-gray-600">{formattedDate}</span>
-                                        </div>
-                                    </li>
-                                )
-                            })}
-                        </ul>
+                    <div className="flex items-start gap-5">
+                        <div className="bg-white rounded shadow p-4 w-full">
+                            <h3 className="text-lg font-semibold mb-2">Usuários Registrados Recentemente</h3>
+                            <ul>
+                                {users && users.length > 0 && users.map((user: any) => {
+                                    const timestamp = new Date(user.email_addresses[0].created_at)
+                                    const day = timestamp.getUTCDate();
+                                    const month = timestamp.getUTCMonth() + 1; // Os meses em JavaScript são baseados em zero
+                                    const year = timestamp.getUTCFullYear();
+                                    const hours = timestamp.getUTCHours();
+                                    const minutes = timestamp.getUTCMinutes();
+                                    const formattedDate = `${day}-${month}-${year} ${hours}:${minutes}`;
+                                    return (
+                                        // <pre>{JSON.stringify(users,null,2)}</pre>
+                                        <li key={user.id} className="flex gap-2 border-b py-2">
+                                            <img src={user.profile_image_url} alt="Foto de perfil" className="w-10 h-10 rounded-full mr-2" />
+                                            <div>
+                                                <span className="block text-gray-800">{user.first_name} {user.last_name}</span>
+                                                <span className="block text-gray-600">{user.email_addresses[0].email_address}</span>
+                                                <span className="block text-gray-600">{formattedDate}</span>
+                                            </div>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+                        <div className="bg-white rounded shadow p-4 w-full">
+                            <h3 className="text-lg font-semibold mb-2">Usuários Logados Recentemente</h3>
+                            <ul>
+                                {users && users.length > 0 && users.map((user: any) => {
+                                    const timestamp = new Date(user.last_sign_in_at)
+                                    const day = timestamp.getUTCDate();
+                                    const month = timestamp.getUTCMonth() + 1; // Os meses em JavaScript são baseados em zero
+                                    const year = timestamp.getUTCFullYear();
+                                    const hours = timestamp.getUTCHours();
+                                    const minutes = timestamp.getUTCMinutes();
+                                    const formattedDate = `${day}-${month}-${year} ${hours}:${minutes}`;
+                                    return (
+                                        // <pre>{JSON.stringify(users,null,2)}</pre>
+                                        <li key={user.id} className="flex gap-2 border-b py-2">
+                                            <img src={user.profile_image_url} alt="Foto de perfil" className="w-10 h-10 rounded-full mr-2" />
+                                            <div>
+                                                <span className="block text-gray-800">{user.first_name} {user.last_name}</span>
+                                                <span className="block text-gray-600">{user.email_addresses[0].email_address}</span>
+                                                <span className="block text-gray-600">{formattedDate}</span>
+                                            </div>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
