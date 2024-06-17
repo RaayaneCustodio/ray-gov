52e0b1226ae5fd656558bdc77eeea587cfa7b201
Raayane Custodio <raayanecustodio@gmail.com>
2024-06-16 22:13:46 -0300

    alteração imagem elizabeth

--- app/(www)/page.tsx
+++ app/(www)/page.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import MyCarousel from "#/components/carousel";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-900 text-white p-6">
       <div className="w-full md:w-1/2 text-center space-y-6">
-        <h1 className="text-4xl md:text-6xl font-bold mb-6 gap-10">Bem-vindo à Gabinete Digital</h1>
-        <p className="text-lg mb-6">Sua plataforma para gestão digital eficiente.</p>
+        <Image
+          src="https://firebasestorage.googleapis.com/v0/b/compartilhai.appspot.com/o/logo%20elizabeth%20branca-1.png?alt=media&token=e2818b58-0551-483d-a5bd-d7059c3f2b7c" // Usando a imagem importada
+          alt="Imagem da postagem"
+          width={500}
+          height={300}
+          className="w-full h-auto rounded cursor-pointer transition hover:opacity-90"
+        />
       </div>
     </main>
   );
