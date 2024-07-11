2deea1266b69d3b2588dc74d1a4d9cdbc11e20a2
Raayane Custodio <raayanecustodio@gmail.com>
2024-07-11 00:28:15 -0300

    Atualizando dados do firebase

--- app/(www)/page.tsx
+++ app/(www)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="w-full md:w-1/2 text-center space-y-6">
         <Image
           src="https://firebasestorage.googleapis.com/v0/b/compartilhai.appspot.com/o/logo%20elizabeth%20branca-1.png?alt=media&token=e2818b58-0551-483d-a5bd-d7059c3f2b7c" // Usando a imagem importada
-          alt="Imagem da postagem"
+          alt="Logo"
           width={500}
           height={300}
           className="w-full h-auto rounded cursor-pointer transition hover:opacity-90"
