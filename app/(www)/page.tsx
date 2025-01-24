ee6240b162be027089e6b74849f5029d06df08c5
Raayane Custodio <raayanecustodio@gmail.com>
2025-01-23 23:03:04 -0300

    nova imagem

--- app/(www)/page.tsx
+++ app/(www)/page.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import MyCarousel from "#/components/carousel";
 import Image from "next/image";
+import juliokuller from "../components/svgs/juliokuller.png"; // Importando a imagem
 
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-900 text-white p-6">
       <div className="w-full md:w-1/2 text-center space-y-6">
         <Image
-          src="https://firebasestorage.googleapis.com/v0/b/compartilhai.appspot.com/o/logo%20elizabeth%20branca-1.png?alt=media&token=e2818b58-0551-483d-a5bd-d7059c3f2b7c" // Usando a imagem importada
-          alt="Logo"
+          src={juliokuller} // Referenciando a imagem importada
+          alt="Imagem Julio"
           width={500}
           height={300}
           className="w-full h-auto rounded cursor-pointer transition hover:opacity-90"
@@ -17,4 +17,3 @@ export default function Home() {
     </main>
   );
 }
-
